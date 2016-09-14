991bc5b71901aa28a66d8d971911184d6937da6c
Swapnil <swapnilcramteke@gmail.com>
2016-09-14 20:27:51 +0530

    Initial Commit

--- HardwareAndSoftwareInventoryService/HardwareAndSoftwareInventoryService.cs
+++ HardwareAndSoftwareInventoryService/HardwareAndSoftwareInventoryService.cs
@@ -19,6 +19,7 @@ namespace HardwareAndSoftwareInventoryService
 
         protected override void OnStart(string[] args)
         {
+            //
         }
 
         protected override void OnStop()
