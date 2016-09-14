5d379a17939590d47498827ca617c157d4770200
Sonal <Sonal@SONAL-PC>
2016-09-14 20:01:19 +0530

    Add project files.

---
+++ HardwareAndSoftwareInventoryService/HardwareAndSoftwareInventoryService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareAndSoftwareInventoryService
+{
+    public partial class HardwareAndSoftwareInventoryService : ServiceBase
+    {
+        public HardwareAndSoftwareInventoryService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
